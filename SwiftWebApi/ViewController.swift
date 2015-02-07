944d56fde31a55cb61e43ed398cbcc10cfac1b2b
何文新 <wesin@hewenxindeMacBook-Pro.local>
2015-02-07 23:04:59 +0800

    Initial Commit

---
+++ SwiftWebApi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftWebApi
+//
+//  Created by 何文新 on 15/2/7.
+//  Copyright (c) 2015年 wesin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
